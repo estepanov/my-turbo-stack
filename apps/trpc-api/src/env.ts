39644ba0651e74564ed580c099b7a9b42a4f0596
Evans A. Stepanov <evans@stepanov.email>
2024-03-31 04:11:46 -0400

    Add skipValidation option to env configuration

--- apps/trpc-api/src/env.ts
+++ apps/trpc-api/src/env.ts
@@ -2,6 +2,11 @@ import { createEnv } from '@t3-oss/env-core'
 import { z } from 'zod'
 import { env as authEnv } from '@myturbostack/auth/env'
 
+const skip =
+  !!process.env.SKIP_ENV_VALIDATION &&
+  (process.env.SKIP_ENV_VALIDATION === 'true' ||
+    process.env.SKIP_ENV_VALIDATION === '1')
+
 export const env = createEnv({
   extends: [authEnv],
   server: {
@@ -13,4 +18,5 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
   },
+  skipValidation: skip,
 })
--- packages/auth/src/env.ts
+++ packages/auth/src/env.ts
@@ -1,6 +1,11 @@
 import { createEnv } from '@t3-oss/env-core'
 import { z } from 'zod'
 
+const skip =
+  !!process.env.SKIP_ENV_VALIDATION &&
+  (process.env.SKIP_ENV_VALIDATION === 'true' ||
+    process.env.SKIP_ENV_VALIDATION === '1')
+
 export const env = createEnv({
   server: {
     NODE_ENV: z
@@ -15,4 +20,5 @@ export const env = createEnv({
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
+  skipValidation: skip,
 })
